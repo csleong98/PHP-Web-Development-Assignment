c4b51090cc6a62b23de2c947f09c391bf9aa15e6
Chee Seng Leong <Chee Seng Leong>
2017-10-17 21:33:20 +0800

    testing

---
+++ contacts.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 10, 2017 at 01:57 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myaddressbook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` int(255) NOT NULL,
+  `contact_name` varchar(255) NOT NULL,
+  `contact_phone` varchar(50) NOT NULL,
+  `contact_email` varchar(255) NOT NULL,
+  `contact_address` text NOT NULL,
+  `contact_gender` varchar(10) NOT NULL,
+  `contact_relationship` varchar(10) NOT NULL,
+  `contact_dob` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `contact_name`, `contact_phone`, `contact_email`, `contact_address`, `contact_gender`, `contact_relationship`, `contact_dob`) VALUES
+(5, 'Chee Seng', '0182711700', 'macintoshleong@gmail.com', 'Jalan High', '1', '2', '2017-10-07'),
+(3, 'dada', '12314927', 'dada@hotmail.com', 'asdjajskd', 'Male', 'Family', '2017-09-04'),
+(4, 'asxdc', 'zxc', '234@hotmail.com', 'askljdsklasjdklasjkljakldj', 'Male', 'Family', '2017-09-04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
