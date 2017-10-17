8398a7f4f9389f146914d993b967165bbc68d86d
Chee Seng Leong <macintoshleong@gmail.com>
2017-10-17 22:23:36 +0800

    Remove duplicated

--- new/connection2.php
+++ new/connection2.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	$conn2 = mysqli_connect("localhost","root","","sign");
 
@@ -9,15 +8,3 @@
 	echo "<script>alert('Connection Successfully!');</script>";
 
 ?>
-=======
-<?php
-	$conn2 = mysqli_connect("localhost","root","","sign");
-
-	if(mysqli_connect_errno())
-	{
-		die("<script>alert('Error in connection!');window.history.go(-1);</script>");
-	}
-	echo "<script>alert('Connection Successfully!');</script>";
-
-?>
->>>>>>> 47c00a2697cf14d1451ae0f223d3e34a8daf04d4
