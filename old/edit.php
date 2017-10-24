c442873db2a8c7c812b30b612afb02ce9ac884f9
LEONG CHEE SENG <TP042237@mail.apu.edu.my>
2017-10-24 08:54:20 +0800

    removed README2 and edited edit.php

--- old/edit.php
+++ old/edit.php
@@ -3,11 +3,26 @@
 
 <head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-<title>Untitled 1</title>
+<title>Edit Form</title>
 </head>
 
 <body>
+<!-- Step 1: use PHP to read database -->
 
+<?php
+
+    /* a. get is from URL */
+    $uid = $_GET['id'];
+    // b. connect to database 
+    include "connection.php";
+    //c. write the sql query
+    $sql = "SELECT * FROM contacts WHERE id = $uid";
+    //d. execute the sql query
+    $result = mysql_query($conn, $sql);
+    //e. get the data from the database into array
+
+?>
+<!-- Step 2: use HTML Form and PHP to display the resut -->
 </body>
 
 </html>
