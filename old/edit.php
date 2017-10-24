67c645f07a4044d97f6b5f11f22f56055d379e24
LEONG CHEE SENG <TP042237@mail.apu.edu.my>
2017-10-24 09:19:34 +0800

    edit.php updated

--- old/edit.php
+++ old/edit.php
@@ -18,11 +18,32 @@
     //c. write the sql query
     $sql = "SELECT * FROM contacts WHERE id = $uid";
     //d. execute the sql query
-    $result = mysql_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);
     //e. get the data from the database into array
-
+    if($rows=mysqli_fetch_array($result));
+    {
 ?>
-<!-- Step 2: use HTML Form and PHP to display the resut -->
+    <!-- Step 2: use HTML Form and PHP to display the resut -->
+    <form method="POST" action="update.php">
+        <table>
+            <tr>
+                <th width="200px">ID</th>
+                <td><input type="text" value="<?php echo $uid;?>" name="uid" readonly></td>
+            </tr>
+            <tr>
+                <th>Name:</th>
+                <td widt"300px">
+                    <input type="text" name="name" value="<?php echo $rows['contact_name']?>" require>
+                </td>
+            </tr>
+        </table>
+    </form>
 </body>
-
+<?php 
+    }
+    else
+    {
+        die("<script>alert('No such user'); window.location.href='view.php'</script>");
+    }
+?>
 </html>
