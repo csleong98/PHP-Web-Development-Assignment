ea29787da2e747d0590ab2d38670fb111f0b2d56
Chee Seng Leong <macintoshleong@gmail.com>
2017-11-21 19:29:37 +0800

    created login.html & .php

---
+++ website/login.php
@@ -0,0 +1,42 @@
+<?php
+
+  session_start();
+
+  include "connection.php";
+
+  $username = mysqli_real_escape_string($conn, $_POST['user']);
+  $password = mysqli_real_escape_string($conn, $_POST['pass']);
+
+  $sql = "SELECT * FROM users WHERE username = '".$username."' AND password = '".md5($password)."';";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result)<=0) {
+
+    $sql = "SELECT * from users WHERE username = '".$username."' AND password = '".$password."'";
+    $result = mysqli_query($conn, $sql);
+
+      if (mysqli_num_rows($result)<=0) {
+        echo "<script>alert('Wrong username / password !Please Try Again!');";
+        die("window.history.go(-1);</script>");
+      }
+  }
+
+  if ($row=mysqli_fetch_array($result)) {
+
+    $_SESSION['user'] = $row['username'];
+    $_SESSION['password'] = $row['password'];
+    $_SESSION['role'] = $row['role'];
+
+  }
+  if ($_SESSION['role']==="1") {
+    echo "<script>alert('Welcome back! ".$_SESSION['user']."');";
+    echo "window.location.href='default.php';</script>";
+  }
+  else if($_SESSION['role'] ==="0") {
+    echo "<script>alert('Welcome back! ".$_SESSION['admin']."');";
+    //echo "window.location.href='admin.html';</script>";
+  }
+  else {
+    //echo "window.location.href='login.html';</script>";
+  }
+ ?>
