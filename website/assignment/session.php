472dc661255ffcff736a6ce3e1854d2decd034b2
Chee Seng Leong <macintoshleong@gmail.com>
2017-11-22 13:39:42 +0800

    deleted php files outside

--- website/assignment/session.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  if (!isset($_SESSION['user'])) {
-    echo "<script>alert('Please login first before using the service!');</script>";
-    die("<script>window.location.href='login.html'</script>");
-  }
-
- ?>
