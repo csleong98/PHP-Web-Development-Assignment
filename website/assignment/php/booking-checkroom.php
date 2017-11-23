00b737336650a2161237d7f44de0a172b214c273
LEONG CHEE SENG <TP042237@mail.apu.edu.my>
2017-11-23 15:40:41 +0800

    updated booking-checkroom.php

--- website/assignment/php/booking-checkroom.php
+++ website/assignment/php/booking-checkroom.php
@@ -6,21 +6,53 @@ include('../php/connection.php');
 
 $username = mysqli_real_escape_string($conn2,$_SESSION['user']);
 
+$roomname =  mysqli_real_escape_string($conn2, $_POST['room']);
 $cit = mysqli_real_escape_string($conn2, $_POST['check-in-time']);
 $cot = mysqli_real_escape_string($conn2, $_POST['check-out-time']);
+
 $days = mysqli_real_escape_string($conn2, $_POST['days']);
 $totalprice = mysqli_real_escape_string($conn2, $_POST['totalprice']);
 
-$sql = "INSERT INTO rooms(bookedUser,checkInTime,". "checkOutTime,days,totalprice,bookDay) values ('$username','$cit','$cot','$days','$totalprice',Now());";
+$sql = "INSERT INTO rooms(bookedUser,roomName,checkInTime,". "checkOutTime,days,totalprice,bookDay) values ('$username','$roomname','$cit','$cot','$days','$totalprice',Now());";
 
-mysqli_query($conn2, $sql);
+$sql2 = "SELECT * FROM rooms WHERE roomName='$roomname'";
 
-if(mysqli_affected_rows($conn2)<=0)
+$result = mysqli_query($conn2, $sql2);
+
+if(mysqli_num_rows($result)<=0) //if no result there
 {
-    die("<script>alert('Cannot insert data!');</script>");
+    die("<script>alert('No data from database!');</script>");
 }
 
-echo "<script>alert('Successfully to insert data!');</script>";
+while($rows = mysqli_fetch_array($result))
+{
+    if($rows['checkInTime'] == $cit)
+    {
+        echo 'same time';
+    }
+    else
+    {
+        echo 'not same';
+    }
+}
+// while ($rows = mysqli_fetch_array($result)) {
+//     if($rows['checkInTime'] == $cit) {
+//         echo 'same time';
+//     }
+//     else {
+//         echo 'not working';
+//     }
+// }
+// echo $result;
+
+// mysqli_query($conn2, $sql);
+
+// if(mysqli_affected_rows($conn2)<=0)
+// {
+//     die("<script>alert('Cannot insert data!');</script>");
+// }
+
+// echo "<script>alert('Successfully to insert data!');</script>";
 
 // $result = mysqli_query($conn, $sql); //run the sql query
 // all the data store in variable result 
