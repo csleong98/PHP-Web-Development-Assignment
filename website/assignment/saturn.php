27189d01389552fa857500f38b12a4ad6c255e7e
Chee Seng Leong <macintoshleong@gmail.com>
2017-11-24 11:55:09 +0800

    updated planets

--- website/assignment/saturn.php
+++ website/assignment/saturn.php
@@ -92,21 +92,11 @@
                 <div class="thumbnail">
                     <img class="img-responsive" src="http://placehold.it/800x300" alt="">
                     <div class="caption-full">
-                        <h4 class="pull-right">$24.99</h4>
-                        <h4>
-                            <a href="#">Product Name</a>
-                        </h4>
-                        <p>See more snippets like these online store reviews at
-                            <a target="_blank" href="http://bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-                        <p>Want to make these reviews work? Check out
-                            <strong>
-                                <a href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this building a review system tutorial</a>
-                            </strong>over at maxoffsky.com!</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                            ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                            sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <h4 class="pull-right">RM150 (per night)</h4>
+                    <h4>
+                        <a href="#">Saturn Room</a>
+                    </h4>
+                    <p>Deep dark blue and cozy.</p>
                     </div>
                 </div>
             </div>
@@ -180,7 +170,7 @@
         function cal() {
             if (document.getElementById("check-in-time")) {
                 document.getElementById("days").value = GetDays();
-                var oriprice = 24.99;
+                var oriprice = 150;
                 var days = parseFloat($("#days").val());
                 $("#totalprice").val(oriprice * days);
             }
